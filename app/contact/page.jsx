2eb629546960501754f7dffd5cc53267428370a3
gokul-MCA <154983453+gokul-MCA@users.noreply.github.com>
2024-11-02 23:42:12 +0530

    image contact

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -3,6 +3,14 @@ import ContactForm from "@/components/ContactForm";
 export const metadata = {
   title: "Contact Us - Portfolio Builder",
   description: "Get in touch with us for inquiries and support.",
+  images: [
+    {
+      url: "https://portfolio-builder-nextjs.netlify.app/contact.jpg",
+      width: 800,
+      height: 600,
+      alt: "Contact Us",
+    },
+  ],
   openGraph: {
     title: "Contact Us - Portfolio Builder",
     description: "Reach out to us for any questions or support you need.",
@@ -17,12 +25,6 @@ export const metadata = {
     ],
     type: "website",
   },
-  // twitter: {
-  //   card: "summary_large_image",
-  //   title: "Contact Us - Portfolio Builder",
-  //   description: "Reach out to us for any questions or support you need.",
-  //   images: ["/contact.jpg"],
-  // },
 };
 
 
